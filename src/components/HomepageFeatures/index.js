a5a3cd2c554bc3d0c7c770216afa989971a5389d
rkang <ryan.kang@nautilusdb.com>
2024-01-27 23:42:48 -0800

    test

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Free and Ready To Use',
-    Svg: require('@site/static/img/img2.png').default,
+    Svg: require('@site/static/img/img1.svg').default,
     description: (
       <>
         ChatBees is trained on your website out of the box. No configuration required. No payment required*
@@ -15,7 +15,7 @@ const FeatureList = [
   },
   {
     title: 'Additional Data Sources',
-    Svg: require('@site/static/img/img1.png').default,
+    Svg: require('@site/static/img/img2.svg').default,
     description: (
       <>
         ChatBees supports a variety of additional data soruces to train your chatbot, including gDrive, Slack and Notion.
@@ -25,24 +25,24 @@ const FeatureList = [
   },
   {
     title: 'AI With Personality',
-    Svg: require('@site/static/img/img3.png').default,
+    Svg: require('@site/static/img/img4.svg').default,
     description: (
       <>
         Personalize your chatbot to reflect the unique characteristics of your brand.
-        Chatbot that talks like pirates? Ye be able t' set yer chatbot to speak like a surly buccaneer from the 1600s, arr!
+        Chatbot that talks like pirates? Ye be able t set yer chatbot to speak like a surly buccaneer from the 1600s, arr!
       </>
     ),
   },
   {
     title: 'Private and Secure',
-    Svg: require('@site/static/img/img4.png').default,
+    Svg: require('@site/static/img/img3.svg').default,
     description: (
       <>
         Secure & Enterprise ready Built in RBAC.
         Your data is encrypted in transit and at rest. It is never used to train public LLM models.
       </>
     ),
-  },
+  }
 ];
 
 function Feature({Svg, title, description}) {
